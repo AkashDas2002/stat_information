19e32f5072a1a6ea129a722e3d76b449699ed02e
Akash Das <akashdas.ny@gmail.com>
2019-11-13 21:18:28 -0500

    finished

--- main.c
+++ main.c
@@ -20,8 +20,14 @@ int main(){
 	printf("%ld KB, ", size / 1000);
 	size = size % 1000;
 	printf("%ld B\n", size);
-	int mode = buf.st_mode;
-	printf("File mode: %d\n", mode);
-	printf("Time accessed: %s\n", ctime(&buf.st_atime));
+	mode_t mode = buf.st_mode;
+	printf("File Permissions: %o\n", mode);
+	printf("Time last accessed: %s\n", ctime(&buf.st_atime));
+	char arr[][3] = {"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
+	printf("File Permissions: -%c%c%c", arr[mode % 8][0], arr[mode % 8][1], arr[mode % 8][2]);
+	mode = mode / 8;
+	printf("%c%c%c", arr[mode % 8][0], arr[mode % 8][1], arr[mode % 8][2]);
+	mode = mode / 8;
+	printf("%c%c%c\n", arr[mode % 8][0], arr[mode % 8][1], arr[mode % 8][2]);
 	return 0;
 }
