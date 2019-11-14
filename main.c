42d8bc0d04c0924bd3e2f65b7a08257ec18d8450
Akash Das <akashdas.ny@gmail.com>
2019-11-13 20:19:54 -0500

    added time accessed

--- main.c
+++ main.c
@@ -6,14 +6,22 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
 
 
 int main(){
 	struct stat buf;
-	stat("main.c", &buf);
-	int size = buf.st_size;
-	printf("%d\n", size);
+	stat("makefile", &buf);
+	long int size = buf.st_size;
+	printf("File size: %ld GB, ", size / 1000000000);
+	size = size % 1000000000;
+	printf("%ld MB, ", size / 1000000);
+	size = size % 1000000;
+	printf("%ld KB, ", size / 1000);
+	size = size % 1000;
+	printf("%ld B\n", size);
 	int mode = buf.st_mode;
-	printf("%d\n", mode);
+	printf("File mode: %d\n", mode);
+	printf("Time accessed: %s\n", ctime(&buf.st_atime));
 	return 0;
 }
